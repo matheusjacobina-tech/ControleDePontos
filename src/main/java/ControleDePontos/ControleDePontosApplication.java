f9f3b6b15e42013d2fbebad699cbfd279d29d682
Matheus Jacobina <71132039+matheusjacobina-tech@users.noreply.github.com>
2021-09-01 20:29:50 -0300

    commit inicial

---
+++ src/main/java/ControleDePontos/ControleDePontosApplication.java
@@ -0,0 +1,13 @@
+package ControleDePontos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDePontosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDePontosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ControleDePontos/ControleDePontosApplicationTests.java
@@ -0,0 +1,13 @@
+package ControleDePontos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDePontosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
