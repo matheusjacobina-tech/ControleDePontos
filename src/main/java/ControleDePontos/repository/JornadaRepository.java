4b6252fff59563bea54e9c01f5c7e703d0df565d
Matheus Jacobina <71132039+matheusjacobina-tech@users.noreply.github.com>
2021-09-02 14:08:32 -0300

    feature api jornada

---
+++ src/main/java/ControleDePontos/repository/JornadaRepository.java
@@ -0,0 +1,9 @@
+package ControleDePontos.repository;
+
+import ControleDePontos.model.JornadaTrabalho;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JornadaRepository extends JpaRepository<JornadaTrabalho, Long> {
+}
